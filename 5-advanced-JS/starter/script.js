9cc68b1dbddef4edf8abbe9f491228cc40ea311a
ericjoneill <42359370+ericjoneill@users.noreply.github.com>
2018-12-05 16:35:08 -0500

    four commit

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -94,29 +94,80 @@
 
 
 
-var Person = function(name, yearOfBirth, job){
-    this.name = name;
-    this.yearOfBirth = yearOfBirth;
-    this.job = job;
+// var Person = function(name, yearOfBirth, job){
+//     this.name = name;
+//     this.yearOfBirth = yearOfBirth;
+//     this.job = job;
+// }
+
+// Person.prototype.calculateAge = function(){
+//     console.log(2016 - this.yearOfBirth)
+// }
+
+// Person.prototype.lastName = 'Smith'
+
+
+// var john = new Person('John', 1990, 'teacher');
+
+// john.calculateAge();
+
+// var jane = new Person('Jane', 1969, 'designer');
+
+// jane.calculateAge()
+
+// var mark = new Person('Mark', 1948, 'retired')
+
+// mark.calculateAge()
+
+// console.log(mark.lastName)
+
+// Object.create
+
+// var personProto = {
+//     calculateAge: function(){
+//         console.log(2016 - this.yearOfBirth)
+//     }
+// }
+
+// var john = Object.create(personProto)
+// john.name = 'John'
+// john.yearOfBirth = 1990;
+// john.job = 'teacher'
+
+// var jane = Object.create(personProto, {
+//     name: { value: 'Jane'},
+//     yearOfBirth: { value: 1969 },
+//     job: { value: 'designer'}
+// })
+
+var years = [1990, 1965, 1937, 2005, 1998]
+
+function arrayCalc(arr, fn){
+    var arrRes = [];
+    for (let i = 0; i < arr.length; i++){
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
 }
 
-Person.prototype.calculateAge = function(){
-    console.log(2016 - this.yearOfBirth)
+function calculateAge(el){
+    return 2016 - el;
+}
+function maxHeartRate(el){
+    if (el >= 18 && el <= 81){
+    return Math.round(206.9 - (0.67 - el));
+    } else{
+        return -1
+    }
 }
 
-Person.prototype.lastName = 'Smith'
+function isFullAge(el){
+    return el >= 18
+}
+
+var ages = arrayCalc(years, calculateAge)
+var fullAges = arrayCalc(ages, isFullAge)
+var rates = arrayCalc(ages, maxHeartRate)
 
 
-var john = new Person('John', 1990, 'teacher');
-
-john.calculateAge();
-
-var jane = new Person('Jane', 1969, 'designer');
-
-jane.calculateAge()
-
-var mark = new Person('Mark', 1948, 'retired')
-
-mark.calculateAge()
-
-console.log(mark.lastName)
+console.log(rates)
