4ce104868bf06e11525ec1b69141c722b6a56ed6
ericjoneill <42359370+ericjoneill@users.noreply.github.com>
2018-12-05 18:40:56 -0500

    six commit

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -140,34 +140,94 @@
 //     job: { value: 'designer'}
 // })
 
-var years = [1990, 1965, 1937, 2005, 1998]
+// var years = [1990, 1965, 1937, 2005, 1998]
 
-function arrayCalc(arr, fn){
-    var arrRes = [];
-    for (let i = 0; i < arr.length; i++){
-        arrRes.push(fn(arr[i]));
+// function arrayCalc(arr, fn){
+//     var arrRes = [];
+//     for (let i = 0; i < arr.length; i++){
+//         arrRes.push(fn(arr[i]));
+//     }
+//     return arrRes;
+// }
+
+// function calculateAge(el){
+//     return 2016 - el;
+// }
+// function maxHeartRate(el){
+//     if (el >= 18 && el <= 81){
+//     return Math.round(206.9 - (0.67 - el));
+//     } else{
+//         return -1
+//     }
+// }
+
+// function isFullAge(el){
+//     return el >= 18
+// }
+
+// var ages = arrayCalc(years, calculateAge)
+// var fullAges = arrayCalc(ages, isFullAge)
+// var rates = arrayCalc(ages, maxHeartRate)
+
+
+// console.log(rates)
+
+
+
+
+// Lecture: Functions returning functions
+
+function interviewQuestion(job){
+    if (job === 'designer'){
+        return function(name){
+            console.log(name + ', can you please explain what UX design is?')
+        }
+    } else if (job === 'teacher'){
+        return function(name){
+        console.log('What subject do you teach, ' + name + '?')
+        }
+    } else {
+        return function(name){
+            console.log('Hello ' + name + ', what do you do?')
+        }
     }
-    return arrRes;
 }
 
-function calculateAge(el){
-    return 2016 - el;
-}
-function maxHeartRate(el){
-    if (el >= 18 && el <= 81){
-    return Math.round(206.9 - (0.67 - el));
+
+var designerQuestion = interviewQuestion('designer')
+var teacherQuestion =  interviewQuestion('teacher')
+
+teacherQuestion('Eric')
+teacherQuestion('Elmo')
+designerQuestion('Shady')
+
+interviewQuestion('hell')('Satan')
+
+
+
+function whatType(t){
+    if (t === 'breeder'){
+        return function(n){
+            console.log(n + ' is a ' + t + ' who is always at the DCC.')
+        }
+    } else if (t === 'pokemaniac'){
+        return function(n){
+            console.log('watch out! ' + n + ' is known as a ' + t + '.')
+        }
+    } else if (t === 'gym leader'){
+        return function(n){
+            console.log('Wow, a ' + t + '! ' + n + ' must raise good pkmn.')
+        }
     } else{
-        return -1
+        return function(n){
+            console.log('I think ' + n + ' is just an ordinary trainer.')
+        }
     }
 }
 
-function isFullAge(el){
-    return el >= 18
-}
 
-var ages = arrayCalc(years, calculateAge)
-var fullAges = arrayCalc(ages, isFullAge)
-var rates = arrayCalc(ages, maxHeartRate)
+var breeder = whatType('breeder');
+var pokemaniac = whatType('pokemaniac');
+var leader = whatType('leader');
 
-
-console.log(rates)
+pokemaniac('Alan')
